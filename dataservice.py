acddcfc2ab4c10e1502f6734ef1a47a90753a962
Hayden-Johnston <hjohnston.g@gmail.com>
2024-02-29 06:19:16 -0800

    Removed flask

--- dataservice.py
+++ dataservice.py
@@ -1,24 +1,21 @@
-from flask import Flask
 import time
-
-app = Flask(__name__)
 
 def main():
     while True:
-        time.sleep(1)
         with open('input.txt', 'r') as file:
             data = file.read()
             if data == "":
                 file.close()
+                time.sleep(1)
                 continue
             file.close()
         with open('input.txt', 'w') as file:
             file.write('')
+            file.close()
         with open('db.txt', 'a') as file:
             file.write(data)
             file.write("\n")
             file.close()
 
 if __name__ == "__main__":
-    app.run()
     main()
