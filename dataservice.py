ce7df144af568ed65207c5c89b592fee0e6535a7
Hayden-Johnston <hjohnston.g@gmail.com>
2024-03-01 13:17:00 -0800

    fixed json input

--- dataservice.py
+++ dataservice.py
@@ -1,21 +1,31 @@
-import time
+from flask import Flask, request, jsonify
+import json, os
 
-def main():
-    while True:
-        with open('input.txt', 'r') as file:
-            data = file.read()
-            if data == "":
-                file.close()
-                time.sleep(1)
-                continue
-            file.close()
-        with open('input.txt', 'w') as file:
-            file.write('')
-            file.close()
-        with open('db.txt', 'a') as file:
-            file.write(data)
-            file.write("\n")
-            file.close()
+json_file = 'db.json'
+
+if not os.path.exists(json_file):
+    with open(json_file, 'w') as file:
+        json.dump([], file)
+        file.close()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def handledata():
+    if request.method == 'GET':
+        with open(json_file, 'r') as file:
+            output_data = json.load(file)
+        return jsonify(output_data)
+    if request.method == 'POST':
+        request_data = request.json
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+        with open(json_file, 'w') as file:
+            data.append(eval(request_data))
+            json.dump(data, file)
+        print(f"Successful data input: {request_data}")
+        return f"Successful data input: {request_data}"
+    
 
 if __name__ == "__main__":
-    main()
+    app.run(port=3000)
